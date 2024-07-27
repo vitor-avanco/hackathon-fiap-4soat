c8b55c120a76a598cb76b02700ca02a6c8cd31b7
vitor_avanco <vitor.avanco@hotmail.com>
2024-07-27 01:32:47 -0300

    chore: alterando versao nodejs

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,15 +1,33 @@
-import { configure as serverlessExpress } from '@vendia/serverless-express';
+import { Handler, Context } from 'aws-lambda';
+import { Server } from 'http';
+import { createServer, proxy } from 'aws-serverless-express';
+import { eventContext } from 'aws-serverless-express/middleware';
+
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
-let cachedServer: any;
+import express from 'express';
 
-export const handler = async (event: any, context: any) => {
+const binaryMimeTypes: string[] = [];
+
+let cachedServer: Server;
+
+async function bootstrapServer(): Promise<Server> {
   if (!cachedServer) {
-    const nestApp = await NestFactory.create(AppModule);
+    const expressApp = express();
+    const nestApp = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+    nestApp.use(eventContext());
     await nestApp.init();
-    cachedServer = serverlessExpress({ app: nestApp.getHttpAdapter().getInstance() });
+    cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
   }
-
-  return cachedServer(event, context);
+  return cachedServer;
 }
+
+export const handler: Handler = async (event: any, context: Context) => {
+  cachedServer = await bootstrapServer();
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
+};
